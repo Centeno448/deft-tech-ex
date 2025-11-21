75fce95ae920b5c5e1cf710a8294884255cee212
Centeno448 <diegocenteno448@gmail.com>
2025-11-21 11:54:22 -0500

    copy inventory file to build folder

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,20 +1,25 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
+import CopyPlugin from "copy-webpack-plugin";
 
 export const mainConfig: Configuration = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/main/index.ts',
+  entry: "./src/main/index.ts",
   // Put your normal webpack config below here
   module: {
     rules,
   },
-  plugins,
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "./src/inventory.txt", to: "../inventory.txt" }],
+    }),
+  ],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
 };
