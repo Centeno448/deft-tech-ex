89e82fa361c12c1132bc1e9c101b0f16ec7fd08f
Centeno448 <diegocenteno448@gmail.com>
2025-11-21 13:12:51 -0500

    add scss suppport

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -20,6 +20,6 @@ export const mainConfig: Configuration = {
     }),
   ],
   resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".scss", ".json"],
   },
 };
